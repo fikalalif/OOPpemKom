5eeafb8d0ddba49c031bbdd685635b687e650e29
fikalalif <fikalgans26@gmail.com>
2024-10-08 16:04:52 +0700

    isi

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,34 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+class Kaka {
+    String nama = "Wisnu";
+    public void fungsi() {
+        System.out.println("minta duit!");
+    }
+}
+
+class Person extends Kaka{
+    final String adik1 = "nanang";
+    private String adik;
+
+    // Getter
+    public String getAdik() {
+        return adik;
+    }
+
+    // Setter
+    public void setAdik(String newAdik) {
+        this.adik = newAdik;
+    }
+}
+
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Person Adk = new Person();
+        Adk.setAdik("John");
+        Adk.fungsi();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.println(Adk.getAdik());
+        System.out.println(Adk.adik1);
+        System.out.println(Adk.adik1+" "+ Adk.nama );
     }
 }
